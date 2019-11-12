86bfbcf102dd8c37581dae1b2b0f1f4fa799b99d
jon <jon@brooks.engin.umich.edu>
2019-11-12 17:15:07 -0500

    modified:   main.m

--- testdata/main.m
+++ testdata/main.m
@@ -8,8 +8,19 @@ kP = fftshift(fftn(fftshift(P)));
 kP2 = zeros(80,80,80);
 kP2(:,:,(end/2-20):(end/2+19)) = kP;
 P2 = fftshift(ifftn(fftshift(kP2)));
+P2 = abs(P2);
 
 % create Localizer.h5 that can be loaded into ~/github/toppeMRI/SlicePlanner/GUI/
 addpath ~/github/toppeMRI/SlicePlanner/LocalizerScan/   % im2hdf
-im2hdr(P2); 
+%im2hdr(P2); 
 
+% object support
+roi = getROI('ROI.h5',1);
+mask = roi2mask(roi,80,4);
+objSupportMask = mask & (P2>0.15*max(P2(:)));
+save objSupportMask objSupportMask
+
+% IV mask
+roi = getROI('ROI.h5',2);
+iv = roi2mask(roi,80,4);
+save iv iv
