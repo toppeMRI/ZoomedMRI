ddc6878e56f1a81199fb21f896ce77ed6c0ef3d7
jon <jon@brooks.engin.umich.edu>
2019-11-12 16:05:33 -0500

    modified:   main.m

--- testdata/main.m
+++ testdata/main.m
@@ -1,7 +1,15 @@
 
-
+% from Ray
 load P51200_res.mat
+
+% zero-pad to isotropic matrix
 P = sqrt(sum(abs(fPS(:,:,:,:,1)).^2,4));
-kP2 = zeros(80,80,80);
 kP = fftshift(fftn(fftshift(P)));
+kP2 = zeros(80,80,80);
 kP2(:,:,(end/2-20):(end/2+19)) = kP;
+P2 = fftshift(ifftn(fftshift(kP2)));
+
+% create Localizer.h5 that can be loaded into ~/github/toppeMRI/SlicePlanner/GUI/
+addpath ~/github/toppeMRI/SlicePlanner/LocalizerScan/   % im2hdf
+im2hdr(P2); 
+
