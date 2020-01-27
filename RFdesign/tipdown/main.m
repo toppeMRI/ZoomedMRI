54849cb853f2480d0cfde8da00bec2ab9cad7b36
Jon-Fredrik Nielsen <jfnielse@umich.edu>
2020-01-27 17:15:35 -0500

    On branch dev modified:   main.m

--- RFdesign/tipdown/main.m
+++ RFdesign/tipdown/main.m
@@ -1,20 +1,36 @@
 % set paths
-%dosetup;
+dosetup;
 
 % design RF pulse and save to 'rfpulse.mat'
 %main_ktCont; 
 
 % load pulse and pad with zeros at beginning and end (TOPPE requires this)
 load rfpulse;
-b1 = [0; b1; 0];
-gx = [0; gx; 0];
-gy = [0; gy; 0];
-gz = [0; gz; 0];
 
 % set scanner hardware specs
 sys = toppe.systemspecs('maxGrad', 5, 'gradUnit', 'Gauss/cm', ...
 	'maxSlew', 20, 'slewUnit', 'Gauss/cm/ms', ...
 	'raster', 4e-6);
 
-% write RF pulse to TOPPE file (module)
+% try to write to file
+% toppe.writemod('rf', b1, 'gx', gx, 'gy', gy, 'gz', gz, 'system', sys, 'ofname', 'tipdown.mod');
+% fails! Because TOPPE requires all waveforms to:
+% (1) start and end at zero, and (2) be on a 4-sample (16us) boundary
+
+b1 = [0; b1; 0];
+gx = [0; gx; 0];
+gy = [0; gy; 0];
+gz = [0; gz; 0];
+
+nextra = 4 - mod(length(b1),4);
+b1 = [b1; zeros(nextra,1)];
+gx = [gx; zeros(nextra,1)];
+gy = [gy; zeros(nextra,1)];
+gz = [gz; zeros(nextra,1)];
+
+% try again
 toppe.writemod('rf', b1, 'gx', gx, 'gy', gy, 'gz', gz, 'system', sys, 'ofname', 'tipdown.mod');
+
+% display the .mod file
+% Does it satisfy PNS limits?
+toppe.plotmod('tipdown.mod');
